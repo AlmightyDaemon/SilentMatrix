b907c466440a20bfcad494b6acb6fc5eb32a6f6b
Almighty <almightydaemon@example.com>
2026-04-29 15:36:46 +0200

    link

--- download-config.js
+++ download-config.js
@@ -1,3 +1,3 @@
 ﻿// Cambia solo este enlace cuando tengas tu GitHub Release listo.
-window.DOWNLOAD_URL = "https://github.com/EXAMPLE/EXAMPLE/releases/download/v1.0/archivo.exe";
+window.DOWNLOAD_URL = "https://github.com/AlmightyDaemon/SilentMatrix/releases/download/v1.0/SilentMatrix.exe";
 
